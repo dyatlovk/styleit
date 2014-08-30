d45787ae3cfff945634b65109c14803a3ecb7ad0
Jupiter <Jupiter@Jupiter-PC>
2014-08-30 23:09:50 +0400

    Миксин для сетки

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!-- © 2014 Kirill Dyatlov -->
+
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7]> <html class="ie7"> <![endif]-->
+<!--[if IE 8]> <html class="ie8"> <![endif]-->
+<!--[if IE 9]> <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html> <!--<![endif]-->
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Стайлит — легкий css-каркас</title>
+
+        <link rel="stylesheet" type="text/css" href="css/styleit.css">  
+    </head>
+    <body>
+        <div class="sidebar column-3">
+            <?php include '/includes/sidebar.html'; ?>
+        </div>
+        <div class="main-content column-9">
+            <?php include '/includes/main-content.html'; ?>
+        </div>
+    </body>
+</html>
